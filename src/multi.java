12251ba5e17a25e0d55eea347d19b6f354112e87
Moon <Moon@192.168.0.130>
2019-08-01 03:53:42 +0900

    v2 입력받는 버전

--- src/multi.java
+++ src/multi.java
@@ -1,22 +1,21 @@
 /*
-  	곱하기 연습
+  	구구단 연습 v2 입력받는 버전
  */
 
+import java.util.Scanner;
 public class multi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 0; // 앞자리
-		int b = 0; // 뒷자리
+		Scanner scan = new Scanner(System.in);
+		int input = 0; // 입력받을 압자리
+		int b = 0; // 뒷자리				
+		System.out.println("Enter the number");
+		input = scan.nextInt();
 		
-		for(a=1; a<10;a++) 
+		for(b=1; b<10; b++) 
 		{
-			for(b=1; b<10;b++) 
-			{
-				System.out.println(a+" x "+b+" = "+a*b);
-				//윈도우에서 ctrl + alt + down arrow 하면 해당 라인 복사
-
-			}
+			System.out.println(input+" * "+b+" = "+input*b); 
 		}		
 	}
 
