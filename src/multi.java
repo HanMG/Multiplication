6597104d8672f60af563718693bdbd903d498c81
Moon <Moon@192.168.0.130>
2019-08-01 03:39:16 +0900

    initial commit;

---
+++ src/multi.java
@@ -0,0 +1,23 @@
+/*
+  	곱하기 연습
+ */
+
+public class multi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 0; // 앞자리
+		int b = 0; // 뒷자리
+		
+		for(a=1; a<10;a++) 
+		{
+			for(b=1; b<10;b++) 
+			{
+				System.out.println(a+" x "+b+" = "+a*b);
+				//윈도우에서 ctrl + alt + down arrow 하면 해당 라인 복사
+
+			}
+		}		
+	}
+
+}
