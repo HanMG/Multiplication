54a2a6a4ede8a510e25877311feb8cda508b61d2
Moon <Moon@192.168.0.130>
2019-08-01 23:37:50 +0900

    v4 1미만 10이상의 값을 입력시 조건문 출력후 다시 값을 입력받게 변화

--- src/multi.java
+++ src/multi.java
@@ -1,5 +1,5 @@
 /*
-  	구구단 연습 v3 값 입력 및 조건문 if
+  	구구단 연습 v4 값 입력 및 조건문 에러시 다시 값을 입력받게 변화
  */
 
 import java.util.Scanner;
@@ -10,20 +10,45 @@ public class multi {
 		Scanner scan = new Scanner(System.in);
 		int input = 0; // 입력받을 압자리
 		int b = 0; // 뒷자리				
-		System.out.print("Enter the number ");
-		input = scan.nextInt();
-		if(input<1) {
-			System.out.println("1 이상의 값을 입력하세요.");
+	
+		/*
+		while(true){
+			System.out.print("Enter the number ");
+			input = scan.nextInt();
+			if(input<1) {
+				System.out.println("1 이상의 값을 입력하세요.");				
+			}
+			else if(input>9) {
+				System.out.println("9 이하의 값을 입력하세요.");
+			}
+			else {
+				for(b=1; b<10; b++) 
+				{
+					System.out.println(input+" * "+b+" = "+input*b); 
+				}
+				break;
+			}			
 		}
-		else if(input>9) {
-			System.out.println("9 이하의 값을 입력하세요.");
-		}
-		else {
-			for(b=1; b<10; b++) 
-			{
-				System.out.println(input+" * "+b+" = "+input*b); 
-			}	
+		*/
+		
+		for(;;) {
+			System.out.print("Enter the number ");
+			input = scan.nextInt();
+			if(input<1) {
+				System.out.println("1 이상의 값을 입력하세요.");				
+			}
+			else if(input>9) {
+				System.out.println("9 이하의 값을 입력하세요.");
+			}
+			else {
+				for(b=1; b<10; b++) 
+				{
+					System.out.println(input+" * "+b+" = "+input*b); 
+				}
+				break;
+			}
 		}			
+		
 	}
 
 }
